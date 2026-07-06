b22965ef4ac21e776f515eea465b79366a6bd5aa
talesnframesofficial-alt <talesnframesofficial@gmail.com>
2026-07-06 12:40:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "https://fliwgaclwfwtdjnxgdtk.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://fliwgaclwfwtdjnxgdtk.supabase.co";
 
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsaXdnYWNsd2Z3dGRqbnhnZHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMTE2NjYsImV4cCI6MjA5ODg4NzY2Nn0.Kaec9_RTC7fXIukvTstyikd3qvBt5i0TpoiLw2gqCiI ;"
+const SUPABASE_ANON_KEY = "sb_publishable_uEMe4tTFepVEKCL1Y__Qrg_hvqxWp1q";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
